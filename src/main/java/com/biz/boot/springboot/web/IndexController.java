4f8cb4a70b30d12428f9461924a7382d08e9f334
hojin880214 <hojin880214@gmail.com>
2021-09-02 13:16:44 +0900

    user 테이블 등록 안됨

--- src/main/java/com/biz/boot/springboot/web/IndexController.java
+++ src/main/java/com/biz/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.biz.boot.springboot.web;
 
+import com.biz.boot.springboot.config.auth.dto.SessionUser;
 import com.biz.boot.springboot.service.posts.PostsService;
 import com.biz.boot.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        System.out.println("====user : "+ user);
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
